d0e5dd72a0f0bd357b59ed9e81b23dd4da8119ba
Jorge David Runza Gualdron <jd.runza@uniandes.edu.co>
2017-08-20 23:24:20 -0500

    Mejora Worker

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,14 @@
-var cacheName = 'metroPWA';
+var cacheName = 'ParisMetro-step-6-1';
+var dataCacheName = 'ParisMetroData-v1';
 var filesToCache = [
   '/',
   '/index.html',
   '/scripts/app.js',
+  '/scripts/idb.js',
   '/styles/inline.css',
   '/images/ic_add_white_24px.svg',
   '/images/ic_refresh_white_24px.svg'
 ];
-
-var dataCacheName = 'metroData-v1';
 
 self.addEventListener('install', function(e) {
   console.log('[ServiceWorker] Install');
@@ -20,28 +20,24 @@ self.addEventListener('install', function(e) {
   );
 });
 
-
 self.addEventListener('activate', function(e) {
   console.log('[ServiceWorker] Activate');
-
   e.waitUntil(
     caches.keys().then(function(keyList) {
       return Promise.all(keyList.map(function(key) {
-        if (key !== cacheName) {
+        if (key !== cacheName && key !== dataCacheName) {
           console.log('[ServiceWorker] Removing old cache', key);
           return caches.delete(key);
         }
       }));
     })
   );
- 
   return self.clients.claim();
 });
 
-
 self.addEventListener('fetch', function(e) {
   console.log('[Service Worker] Fetch', e.request.url);
-  var dataUrl = 'https://query.yahooapis.com/v1/public/yql';
+  var dataUrl = 'https://api-ratp.pierre-grimaud.fr/v3/schedules/';
   if (e.request.url.indexOf(dataUrl) > -1) {
     /*
      * When the request URL contains dataUrl, the app is asking for fresh
